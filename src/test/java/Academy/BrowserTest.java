a1e415e756ed5837dc85bfe177c923fbb92ab642
ronysoumen <43927213+ronysoumen@users.noreply.github.com>
2025-05-02 00:50:44 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -17,13 +17,7 @@ public class BrowserTest {
 	public void getData()
 	{
 		System.out.println("Hello Guys");
-		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		//WebDriver driver =new ChromeDriver();
-		//driver.get("https://sstechbolster94-akcrfphpcpbyduc8.canadacentral-01.azurewebsites.net/webapp/");
-		//String text =driver.findElement(By.cssSelector("h1")).getText();
-		//System.out.println(text);
-		//Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
-		//driver.close();
+		
 	
 		
 		
